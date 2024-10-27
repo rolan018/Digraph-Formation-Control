15b764978948f1bc1e69e9418fd08cf4784f5feb
rolan018 <dadashev.rr@phystech.edu>
2024-10-27 23:03:40 +0300

    checker

---
+++ utils/checkers.py
@@ -0,0 +1,7 @@
+def check_size(item, size):
+    if item.shape != size:
+            raise ValueError(f"item size must be: {size}")
+
+def check_many_size(item1, item2, size):
+    if item1.shape != size or item2.shape != size:
+            raise ValueError(f"item1 or item2 size must be: {size}")
